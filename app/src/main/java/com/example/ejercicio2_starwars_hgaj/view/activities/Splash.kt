03575af2f8c4015172f5dadb159950297b99bc3f
adrianaSung <adriana.herrera@ingenieria.unam.edu>
2023-01-02 14:41:14 -0600

    Agregando Splash, icono y quitando Hardcoding

---
+++ app/src/main/java/com/example/ejercicio2_starwars_hgaj/view/activities/Splash.kt
@@ -0,0 +1,25 @@
+package com.example.ejercicio2_starwars_hgaj.view.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ejercicio2_starwars_hgaj.databinding.ActivitySplashBinding
+import kotlin.concurrent.thread
+
+
+class Splash : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        thread {
+            Thread.sleep(3000)
+            val intent= Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
